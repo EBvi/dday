796d2153442465c33df343fe5113d072f28c2ea8
EBvi <erobeat@gmail.com>
2016-05-27 00:15:12 +0900

    init: 최초 커밋

---
+++ main.js
@@ -0,0 +1,63 @@
+var app = Application.currentApplication();
+app.includeStandardAdditions = true;
+
+var defaultPath = '~/Library/Application\\ Support/AddressBook/Sources/',
+    subPath = app.doShellScript('echo `/bin/ls -1t '+ defaultPath +' | sed -n 1p`'),
+    path = defaultPath + subPath + '/AddressBook-v22.abcddb';
+
+function getSolarDate(ymd) {
+    var o = ymd.split('-');
+
+    app.doShellScript('touch ~/.korean_lunar_calendar');
+    var lc = app.doShellScript('echo `cat ~/.korean_lunar_calendar | grep \"^'+ymd+'\" | sed -n 1p`'),
+        isNew = (!lc ? true : false), result;
+
+    if(isNew) {
+        try {
+            result = app.doShellScript("curl 'http://astro.kasi.re.kr/Life/Knowledge/solar2lunar/convert_daily_l2s.php' -H 'Host: astro.kasi.re.kr' -H 'Accept-Language: ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3' --compressed -H 'DNT: 1' -H 'Referer: http://astro.kasi.re.kr/Life/Knowledge/solar2lunar/convert_daily_l2s.php' -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' --data 'lun_year="+o[0]+"&lun_month="+o[1]+"&lun_day="+o[2]+"&yoon=3'").match(/<td width=500>(.*?)<\/td>/)[1].match(/(\d+)/g).join('-');
+        } catch(e){}
+        app.doShellScript('echo "'+ymd+':'+result+'" >> ~/.korean_lunar_calendar');
+    } else {
+        result = lc.split(':')[1];
+    }
+    return result || '';
+}
+
+var h = {
+    currentYear: new Date().getFullYear(),
+    currentTime: new Date().getTime(),
+    getRemainDays: function(ymd) { return parseInt(h.datediff(new Date(ymd)) / (24*60*60*1000), 10) + 1; },
+    datediff: function(d) { return d.getTime() - h.currentTime; },
+    makeArr: function(str) {
+        return (''+str).split('\r').map(function(v){
+            var o = v.split('|'),
+                name = o[0],
+                year = h.currentYear + ((h.datediff(new Date(h.currentYear+'-'+o[1])) > 0) ? 0 : 1),
+                md = o[1];
+
+            if((/.*:\-\)/).test(name)){
+                var result = (getSolarDate(year+'-'+md)||'').split('-');
+                name = name.replace(':-', '');
+                year = result[0] || '';
+                md = year ? (result[1] + '-' + result[2]) : '';
+            }
+            return {name:name, y:year, md:md, after: h.getRemainDays(year+'-'+md) || '---' };
+        });
+    }
+};
+
+var birthList = h.makeArr( app.doShellScript("sqlite3 "+path+" \"SELECT ZLASTNAME||ZFIRSTNAME||' (생일)' AS name, strftime('%m-%d',datetime(ZBIRTHDAY+strftime('%s', '2001-01-01 00:00:00'),'unixepoch')) AS md FROM ZABCDRECORD WHERE ZBIRTHDAY IS NOT NULL\" -separator '|'") );
+
+var annivList = h.makeArr( app.doShellScript("sqlite3 "+path+" \"SELECT r.ZLASTNAME||r.ZFIRSTNAME||' ('|| REPLACE(d.ZLABEL, '_\\$\!<Anniversary>\!\\$_', '기념일')||')' AS name, strftime('%m-%d',datetime(d.ZDATE+strftime('%s', '2001-01-01 00:00:00'),'unixepoch')) AS md FROM ZABCDCONTACTDATE d INNER JOIN ZABCDRECORD r ON d.ZOWNER=r.Z_PK\" -separator '|'") );
+
+var arr = birthList.concat(annivList).sort(function(a,b){
+    if(a.after === b.after) return 0;
+    if(a.after < b.after) return -1;
+    if(a.after > b.after) return 1;
+});
+
+var r = '<items>';
+arr.map(function(v){
+    r += `<item><title>${v.name} - ${v.after+(v.after==='---'?'':('일 남음'))} (${v.md?v.md:'없음'})</title><subtitle></subtitle></item>`;
+});
+r += '</items>';
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Alfred용 디데이 Workflow
+
+![스크린샷](https://github.com/EBvi/dday/blob/master/screenshot.png?raw=true)
+
+- 연락처에 등록된 생일, 기념일을 검색하여 얼마나 남았는지 보여줍니다
+- 한국천문연구원의 음력 데이터를 기반으로 정확한 **음력 날짜**를 보여줍니다
+- 최소한의 네트워크만을 사용하기 때문에 매우 빠릅니다 (한번 불러온 음력날짜를 저장해둡니다)
+
+## 요구조건
+
+[JXA](https://developer.apple.com/library/mac/releasenotes/InterapplicationCommunication/RN-JavaScriptForAutomation/Articles/Introduction.html)로 작성되었기 때문에 다음과 같은 요구사항이 필요합니다
+
+- OSX 10.11+ (엘 캐피탄 이상)
+- Alfred 3+
+
+## 알려진 이슈
+
+- 특정 조건에 의해 연락처의 경로(~/Library/Application\\ Support/AddressBook/Sources/)가 변경된 경우, 제대로 데이터를 불러오지 못할 수 있습니다
+- 한국천문연구원의 문서를 분석하여 결과를 제공하기 때문에, 해당 페이지가 변경되면 일부 기능이 작동하지 않을 수 있습니다
