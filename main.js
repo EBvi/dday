0aa5e34b49e1e517d4fd28225ed0928754cf99fa
EBvi <erobeat@gmail.com>
2016-08-11 16:50:19 +0900

    add: 프로필 사진 출력

--- main.js
+++ main.js
@@ -28,12 +28,13 @@ var h = {
     currentTime: new Date().getTime(),
     getRemainDays: function(ymd) { return parseInt(h.datediff(new Date(ymd)) / (24*60*60*1000), 10) + 1; },
     datediff: function(d) { return d.getTime() - h.currentTime; },
-    resolver: function(name, year, md) {
+    resolver: function(name, year, md, img) {
         var result = (getSolarDate(year+'-'+md)||'').split('-');
         return {
             name: name.replace(':-', ''),
             year: result[0] || '',
-            md: year ? (result[1] + '-' + result[2]) : ''
+            md: year ? (result[1] + '-' + result[2]) : '',
+            img: img
         };
     },
     makeArr: function(str) {
@@ -41,11 +42,12 @@ var h = {
             var o = v.split('|'), p = {
                 name: o[0],
                 year: h.currentYear + ((h.datediff(new Date(h.currentYear+'-'+o[1])) > 0) ? 0 : 1),
-                md: o[1]
+                md: o[1],
+                img: o[2]
             };
 
             if((/.*:\-\)/).test(p.name)){
-                p = h.resolver(p.name, h.currentYear, p.md);
+                p = h.resolver(p.name, h.currentYear, p.md, p.img);
                 h.getRemainDays(p.year+'-'+p.md) <= 0 && (p=h.resolver(p.name, h.currentYear+1, p.md));
             }
             p.after = h.getRemainDays(p.year+'-'+p.md) || '---';
@@ -54,9 +56,9 @@ var h = {
     }
 };
 
-var birthList = h.makeArr( app.doShellScript("sqlite3 "+path+" \"SELECT ZLASTNAME||ZFIRSTNAME||' (생일)' AS name, strftime('%m-%d',datetime(ZBIRTHDAY+strftime('%s', '2001-01-01 00:00:00'),'unixepoch')) AS md FROM ZABCDRECORD WHERE ZBIRTHDAY IS NOT NULL\" -separator '|'") );
+var birthList = h.makeArr( app.doShellScript("sqlite3 "+path+" \"SELECT ZLASTNAME||ZFIRSTNAME||' (생일)' AS name, strftime('%m-%d',datetime(ZBIRTHDAY+strftime('%s', '2001-01-01 00:00:00'),'unixepoch')) AS md, ZUNIQUEID AS img FROM ZABCDRECORD WHERE ZBIRTHDAY IS NOT NULL\" -separator '|'") );
 
-var annivList = h.makeArr( app.doShellScript("sqlite3 "+path+" \"SELECT r.ZLASTNAME||r.ZFIRSTNAME||' ('|| REPLACE(d.ZLABEL, '_\\$\!<Anniversary>\!\\$_', '기념일')||')' AS name, strftime('%m-%d',datetime(d.ZDATE+strftime('%s', '2001-01-01 00:00:00'),'unixepoch')) AS md FROM ZABCDCONTACTDATE d INNER JOIN ZABCDRECORD r ON d.ZOWNER=r.Z_PK\" -separator '|'") );
+var annivList = h.makeArr( app.doShellScript("sqlite3 "+path+" \"SELECT r.ZLASTNAME||r.ZFIRSTNAME||' ('|| REPLACE(d.ZLABEL, '_\\$\!<Anniversary>\!\\$_', '기념일')||')' AS name, strftime('%m-%d',datetime(d.ZDATE+strftime('%s', '2001-01-01 00:00:00'),'unixepoch')) AS md, r.ZUNIQUEID AS img FROM ZABCDCONTACTDATE d INNER JOIN ZABCDRECORD r ON d.ZOWNER=r.Z_PK\" -separator '|'") );
 
 var items = [], arr = birthList.concat(annivList).sort(function(a,b){
     if(a.after === b.after) return 0;
@@ -64,6 +66,7 @@ var items = [], arr = birthList.concat(annivList).sort(function(a,b){
     if(a.after > b.after) return 1;
 }).map(function(v){
     items.push({
+        icon: {path: v.img ? `${defaultPath.replace('\\','')+subPath}/Images/${(v.img||'').split(":")[0]}` : ''},
         title:`${v.name} - ${v.after+(v.after==='---' ? '' : ('일전'))}`,
         subtitle: `${v.md ? (v.year+'-'+v.md) : '없음'}`
     });
